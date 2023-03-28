9504e518f0f7f78b6a5fbb707a0cc73a63e6b526
Tyler Tucker <tylertucker202@gmail.com>
2023-03-28 12:30:12 -1000

    got urls mixed up

--- src/api.tsx
+++ src/api.tsx
@@ -8,7 +8,7 @@ const TEL_API_URL = "/api/telSchedule?cmd=getEmployee"
 
 const IS_PRODUCTION: boolean = process.env.REACT_APP_ENVIRONMENT === 'production'
 const IS_TEST: boolean = process.env.REACT_APP_TEST === 'test'
-const API_URL = IS_TEST? "/api/pp/" : "/api_test/pp/"
+const API_URL = IS_TEST? "/api_test/pp/" : "/api/pp/"
 console.log('API_URL set to ', API_URL)
 console.log('IS_PRODUCTION set to ', IS_PRODUCTION)
 
