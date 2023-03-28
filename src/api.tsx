bd4c6d81e50e6c70d1a79632b906a6797bb63db4
Tyler Tucker <tylertucker202@gmail.com>
2023-03-28 12:01:46 -1000

    use test database if testbuild

--- src/api.tsx
+++ src/api.tsx
@@ -7,7 +7,9 @@ import axios, { AxiosError, AxiosResponse } from 'axios'
 const TEL_API_URL = "/api/telSchedule?cmd=getEmployee"
 
 const IS_PRODUCTION: boolean = process.env.REACT_APP_ENVIRONMENT === 'production'
-const API_URL = IS_PRODUCTION ? "/api/pp/" : "/api_test/pp/"
+const IS_TEST: boolean = process.env.REACT_APP_TEST === 'test'
+const API_URL = IS_TEST? "/api/pp/" : "/api_test/pp/"
+console.log('API_URL set to ', API_URL)
 
 export interface User {
     Status: string,
