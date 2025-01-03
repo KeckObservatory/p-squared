7a97059dd5353ad8da5b8799364942bb012ba07e
Tyler Tucker <tylertucker202@gmail.com>
2025-01-03 11:12:07 -1000

    h3 for errors

--- src/add_shifts_dialog.tsx
+++ src/add_shifts_dialog.tsx
@@ -183,7 +183,7 @@ export const AddShiftsDialog = (props: Props) => {
         </DialogTitle>
         <DialogContent>
           {errMsg && (
-            <Typography sx={{ color: 'red' }} variant="caption">{errMsg}</Typography>
+            <Typography sx={{ color: 'red' }} variant="h3">{errMsg}</Typography>
           )}
           <ShiftEntryForm
             ref={childStateRef}
