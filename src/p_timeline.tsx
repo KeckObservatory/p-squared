247e240d0f0878f7573407ef33ce9f3b19cca274
Tyler Tucker <tylertucker202@gmail.com>
2025-10-07 20:36:12 +0100

    ...always use HT for pink line

--- src/p_timeline.tsx
+++ src/p_timeline.tsx
@@ -93,9 +93,9 @@ export const PTimeline = (props: Props) => {
     console.log('get_now_in_hawaii', get_now_in_hawaii().format(), get_now_in_hawaii().unix())
     const lct = new Date()
     const tzoffset = lct.getTimezoneOffset() * 10 * 60 * 1000; //offset in milliseconds
-    const utcunix = lct.getTime() + tzoffset
+    const utcunix = lct.getTime() - tzoffset
     const hitime = utcunix - 10 * 60 * 60 * 1000
-    console.log('local time', lct, 'utc', utcunix, 'hawaii', new Date(hitime))
+    console.log('local time', lct, 'offset', tzoffset, 'utc', new Date(utcunix), 'hawaii', new Date(hitime))
     const [state, setState] = useQueryParam('state', withDefault(ObjectParam, init_state as any))
     const [groups, setGroups] = React.useState([...init_groups])
     const [items, setItems] = React.useState(init_items)
