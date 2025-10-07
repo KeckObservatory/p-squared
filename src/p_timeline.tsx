345dd46415262a650ddda22110d2bcd521ed5c29
Tyler Tucker <tylertucker202@gmail.com>
2025-10-07 09:09:58 -1000

    ...always use HT for pink line

--- src/p_timeline.tsx
+++ src/p_timeline.tsx
@@ -13,7 +13,7 @@ import './p_timeline.css'
 import React, { useEffect } from 'react'
 import Button from '@mui/material/Button'
 import { delete_entry_by_id, get_entries_by_date_range, get_holidays } from './api'
-import { ControlState, Employee, EntryState, DATE_FORMAT, HAWAII_TIMEZONE } from './control'
+import { ControlState, Employee, EntryState, DATE_FORMAT, HAWAII_TIMEZONE, DATETIME_FORMAT } from './control'
 import Paper from '@mui/material/Paper'
 import {
     make_employee_groups,
@@ -55,13 +55,13 @@ interface State {
     unit: Unit
 }
 
-// const get_now_in_hawaii = (tz?: string) => {
-//     const hereAndNow = tz ? dayjs().tz(tz).format(DATETIME_FORMAT) : dayjs().format(DATETIME_FORMAT)  
-//     const HIDate = dayjs().tz(HAWAII_TIMEZONE)
-//     const diff = HIDate.diff(hereAndNow, 'hour')
-//     const nowInHawaii = dayjs(hereAndNow, DATETIME_FORMAT).add(diff, 'hour')
-//     return nowInHawaii
-// }
+const get_now_in_hawaii = (tz?: string) => {
+    const hereAndNow = tz ? dayjs().tz(tz).format(DATETIME_FORMAT) : dayjs().format(DATETIME_FORMAT)  
+    const HIDate = dayjs().tz(HAWAII_TIMEZONE)
+    const diff = HIDate.diff(hereAndNow, 'hour')
+    const nowInHawaii = dayjs(hereAndNow, DATETIME_FORMAT).add(diff, 'hour')
+    return nowInHawaii
+}
 
 export const PTimeline = (props: Props) => {
 
@@ -90,6 +90,7 @@ export const PTimeline = (props: Props) => {
 
     const nowInHawaii = dayjs().tz(HAWAII_TIMEZONE)
     console.log('nowInHawaii', nowInHawaii.format(), nowInHawaii.unix(), 'utc', dayjs.utc().unix(), 'diff', nowInHawaii.unix() - dayjs.utc().unix())
+    console.log('get_now_in_hawaii', get_now_in_hawaii().format(), get_now_in_hawaii().unix())
     const [state, setState] = useQueryParam('state', withDefault(ObjectParam, init_state as any))
     const [groups, setGroups] = React.useState([...init_groups])
     const [items, setItems] = React.useState(init_items)
